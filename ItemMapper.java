ac4d718d7454ddcc2d96aeb47ab6782b4e6040bd
Emily-KR <ek074@hdm-stuttgart.de>
2020-05-25 17:15:24 +0200

    ItemMapper angelegt

---
+++ ItemMapper.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Vector;
+
+public class ItemMapper {
+
+
+	/**Klasse ItemMapper als Singleton
+	 *Variable durch <code> static </code> nur einmal f�r Instanzen der Klassen
+	 *vorhanden
+	 *Sie speichert einzige Instanz der Klasse **/
+	private static ItemMapper itemMapper = null;
+
+    
+}
