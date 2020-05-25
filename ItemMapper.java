788897b46797255bf5a43bf0734ec32454d01e10
Emily-KR <ek074@hdm-stuttgart.de>
2020-05-25 17:16:52 +0200

    ItemMapper Konstruktor

--- ItemMapper.java
+++ ItemMapper.java
@@ -14,5 +14,24 @@ public class ItemMapper {
 	 *Sie speichert einzige Instanz der Klasse **/
 	private static ItemMapper itemMapper = null;
 
+
+    /** Konstruktor gesch�tzt, es kann keine neue Instanz dieser Klasse mit <code>new</code> erzeugt werden**/
+
+	protected ItemMapper() {
+	}
+
+/**Aufruf der statischen Methode durch <code>ArticleMapper.articleMapper()</code>. Singleton: Es kann nur eine 
+ *Instanz von <code>ArticleMapper</code> existieren
+ * @return articleMapper
+ * 
+ */
+
+	public static ItemMapper itemMapper() {
+		if (itemMapper== null) {
+			itemMapper= new ItemMapper();
+		}
+		return itemMapper;
+	}
+
     
 }
